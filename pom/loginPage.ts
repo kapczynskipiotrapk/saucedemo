3ee9c819fd0550c8cec189f977319a9decd1f2df
Piotrek <kapczynskipiotr.wwa@gmail.com>
2026-06-04 10:28:53 +0200

    product sort scenario fix

--- pom/loginPage.ts
+++ pom/loginPage.ts
@@ -1,31 +1,32 @@
 import { expect, Locator, Page } from "@playwright/test"
 
 export class LoginPage {
-    page: Page
-       readonly loginErrorMessage: Locator
+  page: Page
+  readonly loginErrorMessage: Locator
 
-   readonly userNameInput: Locator
-    readonly userPasswordInput: Locator
-    readonly loginButton: Locator
-    constructor(page: Page) {
-        this.page = page
-        this.userNameInput = page.locator('#user-name');
-        this.userPasswordInput = page.locator('#password');
-        this.loginButton = page.locator('#login-button');
-        this.loginErrorMessage = page.locator('[data-test="error"]');
-    }
+  readonly userNameInput: Locator
+  readonly userPasswordInput: Locator
+  readonly loginButton: Locator
+  
+  constructor(page: Page) {
+    this.page = page
+    this.userNameInput = page.locator('#user-name');
+    this.userPasswordInput = page.locator('#password');
+    this.loginButton = page.locator('#login-button');
+    this.loginErrorMessage = page.locator('[data-test="error"]');
+  }
 
-    public async goTo() {
-       await this.page.goto("/")
-    }
+  public async goTo() {
+    await this.page.goto("/")
+  }
 
-public async login(userName: string, password: string) {
-  await this.userNameInput.fill(userName);
-  await this.userPasswordInput.fill(password);
-  await this.loginButton.click()
-}
+  public async login(userName: string, password: string) {
+    await this.userNameInput.fill(userName);
+    await this.userPasswordInput.fill(password);
+    await this.loginButton.click()
+  }
 
-    public async expectErrorMessageIsDisplayed() {
+  public async expectErrorMessageIsDisplayed() {
     await expect(this.loginErrorMessage).toHaveText("Epic sadface: Sorry, this user has been locked out.");
   }
 
